c08f72267487fa5effc4966321d20468941398f2
Ruhtra <kawanarthurskate@gmail.com>
2023-02-22 16:02:21 -0300

    Files Organization

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,7 @@
-import Game from './game.js'
+import Game from '../game.js'
 import { EngineGame } from './modules/main/EngineGame.js';
 import { ListenInput } from './modules/main/ListenInput.js'
+import { Messages } from './modules/main/Messages.js'
 
 
 const game = new Game()
@@ -9,6 +10,10 @@ const socket = new io()
 const engineGame = new EngineGame(game)
 new ListenInput(socket, game)
 
+
+const messages = new Messages(document.querySelector('#messages'))
+messages.add({id: 0, name: 'PlayerOne', text: 'Esse é um texto teste parar ser adicionado'})
+messages.add({id: 1, name: 'PlayerTwo', me:true, text: 'Second4'})
 
 // Sockets
 socket.on('updateGame', (state) => {
